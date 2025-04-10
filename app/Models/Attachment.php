63b14a7714d7f3f2741366506b6dfe64bac94d97
Darkin <lordcigelsalvar@gmail.com>
2025-04-10 16:28:59 +0800

    Refactor Attachment model to correct fillable attributes and enhance file deletion logic

--- app/Models/Attachment.php
+++ app/Models/Attachment.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
@@ -14,8 +15,24 @@ class Attachment extends Model
         'remarks',
         'files',
         'paths',
-        'attachable',
+        'attachable_id',
+        'attachable_type',
     ];
+
+    protected $casts = [
+        'files' => 'collection',
+        'paths' => 'collection',
+    ];
+
+    public static function booted(): void
+    {
+        static::deleting(fn(self $attachment) => $attachment->purge());
+    }
+
+    public function purge(): void
+    {
+        $this->files->each(fn($file) => Storage::delete($file));
+    }
 
     public function attachable(): MorphTo
     {
